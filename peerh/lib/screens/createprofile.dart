2bca7126faef6f144b5728abfbe8d3f61ab64698
Rakesh <mandalrakesh628@gmail.com>
2021-12-29 00:58:02 +0530

    some changed

--- peerh/lib/screens/createprofile.dart
+++ peerh/lib/screens/createprofile.dart
@@ -1,1 +1,107 @@
+import 'package:flutter/material.dart';
 
+class CreateProfilePage extends StatefulWidget {
+  const CreateProfilePage({Key? key}) : super(key: key);
+
+  @override
+  _CreateProfilePageState createState() => _CreateProfilePageState();
+}
+
+class _CreateProfilePageState extends State<CreateProfilePage> {
+  final GlobalKey<FormState> createProfileKey = new GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xff79201B),
+        appBar: AppBar(
+          title: Text(
+            'Create Your Profile',
+            style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Color(0xffD7A74F)),
+          ),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: 800,
+              // height: MediaQuery.of(context).size.width * 0.8,
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
+              child: Form(
+                key: createProfileKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30, left: 20),
+                      child: Text(
+                        "Full Name",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5),
+                      child: TextFormField(
+                        decoration:
+                            InputDecoration(border: OutlineInputBorder()),
+                      ),
+                    ),
+                    /////
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30, left: 20),
+                      child: Text(
+                        "Phone Number",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5),
+                      child: TextFormField(
+                        decoration:
+                            InputDecoration(border: OutlineInputBorder()),
+                      ),
+                    ),
+                    /////
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30, left: 20),
+                      child: Text(
+                        "Address",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5),
+                      child: TextFormField(
+                        decoration:
+                            InputDecoration(border: OutlineInputBorder()),
+                      ),
+                    ),
+                    ////
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30, left: 20),
+                      child: Text(
+                        "State",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5),
+                      child: TextFormField(
+                        decoration:
+                            InputDecoration(border: OutlineInputBorder()),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
+  }
+}
